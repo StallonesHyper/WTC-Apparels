fe5a13cf1ba5f64dd52980c97b87b2fb70444ad5
StallonesHyper <stallonefernandess@gmail.com>
2022-06-23 14:17:50 +0530

    Routing Phase

--- src/Components/Womens.jsx
+++ src/Components/Womens.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import NavBar from "./NavBar";
 
 function Womens() {
   return (
     <div className="Womens-Section">
-      <NavBar></NavBar>
-      
+      <h1>Women's Section</h1>
     </div>
   );
 }
